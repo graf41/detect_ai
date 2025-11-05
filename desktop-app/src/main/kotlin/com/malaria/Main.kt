1ca33c18dd99a0db8a5d51c435c1b2a3af46fdd8
Danila <danila.kokoreff@yndex.ru>
2025-11-05 20:25:41 +0300

    Изменен Main, добавлена возможность нажимать на кнопки, кнопки сами по себе еще пустые

--- desktop-app/src/main/kotlin/com/malaria/Main.kt
+++ desktop-app/src/main/kotlin/com/malaria/Main.kt
@@ -7,9 +7,12 @@ import com.malaria.screens.MainScreen
 import com.malaria.screens.AnalyzeScreen
 import com.malaria.screens.HistoryScreen
 import com.malaria.screens.AboutScreen
+import com.malaria.screens.DatePickerDialog
 
 fun main() = application {
     var currentScreen by remember { mutableStateOf("main") }
+    var showStartDatePicker by remember { mutableStateOf(false) }
+    var showEndDatePicker by remember { mutableStateOf(false) }
 
     Window(
         onCloseRequest = ::exitApplication,
@@ -24,10 +27,30 @@ fun main() = application {
             "analyze" -> AnalyzeScreen(onBackClick = { currentScreen = "main" })
             "history" -> HistoryScreen(
                 onBackClick = { currentScreen = "main" },
-                onStartDateClick = { /* TODO: добавить функционал */ },
-                onEndDateClick = { /* TODO: добавить функционал */ }
+                onStartDateClick = { showStartDatePicker = true },
+                onEndDateClick = { showEndDatePicker = true }
             )
             "about" -> AboutScreen(onBackClick = { currentScreen = "main" })
+        }
+
+        if (showStartDatePicker) {
+            DatePickerDialog(
+                onDateSelected = {
+                    showStartDatePicker = false
+                },
+                onCancel = { showStartDatePicker = false },
+                title = "Выбор начальной даты"
+            )
+        }
+
+        if (showEndDatePicker) {
+            DatePickerDialog(
+                onDateSelected = {
+                    showEndDatePicker = false
+                },
+                onCancel = { showEndDatePicker = false },
+                title = "Выбор конечной даты"
+            )
         }
     }
 }
